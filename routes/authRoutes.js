3e91600f502e4d1fa3d2ede157c4647696a337f7
armansoltani <arman.t.soltani@gmail.com>
2020-11-20 15:28:08 +0100

    [/login/google] validation du token google [/login/google] utilisation des infos du ticket google pour créer le nouvelle utilisateur

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -25,20 +25,57 @@ const createNewUser = (nom, prenom, photoURL, googleID) => {
     }).save()
 }
 
+// récupération du bearer token via le bearerHeader (clef: Authorization ou authorization)
+const getBearerToken = (bearerHeader) => {
+    if (!bearerHeader) {
+        throw "Authorization header introuvable"
+    }
+    const bearer = bearerHeader.split(' ');
+    if (bearer.length !== 2 || bearer[1] === "") {
+        throw "Format du Bearer Token invalide"
+    }
+    return bearer[1]
+}
+
 // routes
 router.post("/login/google", async (req, res) => {
-    const { devName, photoURL } = req.body
-
-    createNewUser("dev", devName, photoURL, "")
-        .then((newUser) => {
-        console.log("[/login/google] Nouvelle utilisateur créé: " + newUser)
-        const token = createToken(newUser._id, jwtAccessTokenMaxAge)
-        res.status(201).json({ jwt: token })
-    })
-        .catch((error) => {
+    // récupération du bearer token (= le token Google)
+    let googleToken
+    try {
+        googleToken = getBearerToken(req.headers.authorization)
+    }
+    catch (error) {
         console.error("[/login/google] " + error)
-        res.status(500).json({ erreur: "Erreur lors de la création de l'utilisateur"})
-    })
+        res.status(400).json({ erreur: error })
+        return
+    }
+
+    // validation du token et récupération des info dans le ticket
+    let ticket
+    try {
+        ticket = await googleClient.verifyIdToken({
+            idToken: googleToken,
+            audience: process.env.CLIENT_ID
+        });
+    }
+    catch (error) {
+        console.error("[/login/google] " + error)
+        res.status(400).json({ erreur: "Token Google invalide" })
+        return
+    }
+
+    // Creation du nouvelle utilisateur
+    const googleUserInfo = ticket.getPayload();
+    createNewUser(googleUserInfo.family_name, googleUserInfo.given_name, googleUserInfo.picture,googleUserInfo.sub)
+        .then((newUser) => {
+            console.log("[/login/google] Nouvelle utilisateur créé: " + newUser)
+            const token = createToken(newUser._id, jwtAccessTokenMaxAge)
+            res.status(201).json({ jwt: token })
+        })
+        .catch((error) => {
+            console.error("[/login/google] " + error)
+            res.status(500).json({ erreur: "Erreur lors de la création de l'utilisateur"})
+        })
 })
 
 module.exports = router;
