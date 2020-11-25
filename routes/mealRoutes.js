a85934cc8f270125b30a1dbafffb5a3d960f2c72
armansoltani <arman.t.soltani@gmail.com>
2020-11-25 15:03:44 +0100

    [GET /repas/:id] Ajout de la route permettant de récupérer un repas à partir d'une id

--- routes/mealRoutes.js
+++ routes/mealRoutes.js
@@ -3,6 +3,7 @@ const router = require("express").Router()
 const { requireBearerToken, requireValidAccessToken } = require("../middlewares/authMiddleware")
 const { requireValidNewMealData } = require("../middlewares/mealMiddleware")
 const Meal = require("../models/Meal")
+const User = require("../models/User")
 
 
 // routes
@@ -34,5 +35,61 @@ router.post("/", requireBearerToken, requireValidAccessToken, requireValidNewMea
             res.status(500).json({ erreur: "Erreur lors de la création du repas"})
         })
 })
+
+router.get("/:id", requireBearerToken, requireValidAccessToken, async (req, res) => {
+    const mealId = req.params.id
+
+    try {
+        const meal = await Meal.findById(mealId)
+        if (meal) {
+            // On test si le repas est toujours actif
+            if (!meal.actif) {
+                console.error(`[repas/:id] Le repas ${mealId} n'est plus actif`)
+                res.status(400).json({ erreur: "Le repas n'est plus actif" })
+            }
+            else {
+                const cook = await User.findById(meal.idCuisinier)
+                if (cook) {
+                    // Si on arrive ici le repas est valide -> on peut envoyer le résultat
+                    const mealData = {
+                        id: meal._id,
+                        photoBase64: meal.photoBase64,
+                        date: meal.date,
+                        tarif: meal.tarif,
+                        description: meal.description,
+                        nbPersonnesMax: meal.nbPersonnesMax,
+                        coordonneesLong: meal.coordonneesLong,
+                        coordonneesLat: meal.coordonneesLat,
+                        regimes: meal.regimes,
+                        allergies: meal.allergies,
+                        cuisinier: {
+                            id: cook._id,
+                            photoURL: cook.photoURL,
+                            email: cook.email,
+                            nom: cook.com,
+                            prenom: cook.prenom
+                        }
+                    }
+
+                    res.status(200).json(mealData)
+
+                } else {
+                    console.error(`[repas/:id] Le cuisinier ${meal.idCuisinier} du repas ${mealId} n'existe pas`)
+                    res.status(404).json({erreur: "Le repas est invalide"})
+                }
+            }
+        }
+        else {
+            console.error(`[repas/:id] Le repas ${mealId} n'existe pas`)
+            res.status(404).json({ erreur: "Le repas n'existe pas" })
+        }
+    }
+    catch (error) {
+        console.error("[repas/:id] " + error)
+        res.status(500).json({ erreur: "Erreur lors de la récupération des informations du repas"})
+    }
+})
+
+
 
 module.exports = router;
